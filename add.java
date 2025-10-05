70fc575e17ed5cfd0141e677f5392d9d2678a260
Hrushi94 <rushikalepatil7@gmail.com>
2025-10-05 11:06:21 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class add{
+    public static void main(String[]args){
+        int a=3;
+        int b=8;
+        int sum =a+b;
+        System.out.println("Result:"+sum);
+    }
+}
