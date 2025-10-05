4a375baab5c44785cfe692366eaccb27c6c66ffe
Hrushi94 <rushikalepatil7@gmail.com>
2025-10-05 11:09:53 +0530

    sub.java

---
+++ sub.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sub{
+    public static void main(String[]args){
+        int a=20;
+        int b=2;
+        int sub =a/b;
+        System.out.println("Result:"+sub);
+    }
+}
