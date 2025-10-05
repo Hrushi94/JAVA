f25680726bfa382d7645b683c217dd4fbe5c1695
Hrushi94 <rushikalepatil7@gmail.com>
2025-10-05 11:19:35 +0530

    multi.java

---
+++ multi.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class multi{
+    public static void main(String[]args){
+        int a=20;
+        int b=2;
+        int multi =a*b;
+        System.out.println("Result:"+multi);
+    }
+}
