0971b3331f68a6bc1a2c5901ba22b0db3564e8a5
Hrushi94 <rushikalepatil7@gmail.com>
2025-10-05 11:21:06 +0530

    div.java

---
+++ div.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class div{
+    public static void main(String[]args){
+        int a=20;
+        int b=2;
+        int div =a-b;
+        System.out.println("Result:"+div);
+    }
+}
